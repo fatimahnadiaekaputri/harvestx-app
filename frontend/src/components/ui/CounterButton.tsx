3c405abc6e2f29d1519ed776ae5cbcef80ed8a41
fatimahnadiaekaputri <fatimahnadiaep@gmail.com>
2025-05-30 00:02:00 +0700

    fix: counterbutton

--- frontend/src/components/ui/CounterButton.tsx
+++ frontend/src/components/ui/CounterButton.tsx
@@ -20,7 +20,7 @@ export default function CounterButton({count, onChange}: CounterButtonProps) {
           Tambah
         </button>
       ) : (
-        <div className="flex items-center border-2 border-black rounded-lg lg:px-6 px-2 md:py-3 py-1 text-lg font-semibold lg:space-x-4 space-x-2">
+        <div className="flex items-center border-2 border-black rounded-lg lg:px-4 px-2 md:py-2 py-1 text-lg font-semibold lg:space-x-4 space-x-2">
           <button onClick={() => onChange(count - 1)}
           >
             <LuCircleMinus className="md:w-10 md:h-10 h-5 w-5" strokeWidth={1}/>
